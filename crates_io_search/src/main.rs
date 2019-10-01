b125edd1b099e6a76c0f17ba4dbcdccff7fea8bf
dalance <dalance@gmail.com>
2019-10-01 18:24:18 +0900

    Update output format

--- crates_io_search/src/main.rs
+++ crates_io_search/src/main.rs
@@ -1,4 +1,19 @@
+use chrono::Utc;
 use crates_io_api::SyncClient;
+use std::env;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+static BLACKLIST: &'static [&'static str] = &[
+    "glium",
+    "console_error_panic_hook",
+    "textwrap",
+    "abscissa",
+    "syntex_errors",
+    "amethyst_error",
+    "tk-listen",
+    "lark-error",
+];
 
 fn main() {
     let client = SyncClient::new();
@@ -9,12 +24,15 @@ fn main() {
             let name = c.name;
             let created = c.created_at;
             let updated = c.updated_at;
+            if BLACKLIST.contains(&name.as_str()) {
+                continue;
+            }
             if let Some(d) = c.description {
                 if d.to_lowercase().contains("error") {
                     let rev = client.crate_reverse_dependencies(&name);
                     if let Ok(rev) = rev {
                         let total = rev.meta.total;
-                        if total != 0 {
+                        if total >= 10 {
                             result.push((total, name, created, updated));
                         }
                     }
@@ -25,15 +43,32 @@ fn main() {
 
     result.sort_by_key(|x| x.0);
     result.reverse();
-    println!("|crate|dependents|created_at|updated_at|");
-    println!("|-----|----------|----------|----------|");
-    for (total, name, created, updated) in result {
-        println!(
-            "|{}|{}|{}|{}|",
-            name,
-            total,
-            created.format("%Y-%m-%d"),
-            updated.format("%Y-%m-%d")
-        );
+
+    let date = Utc::today();
+
+    let file = format!("{}/results.toml", env::var("CARGO_MANIFEST_DIR").unwrap());
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(file)
+        .unwrap();
+
+    for (dependents, name, created, updated) in result {
+        let _ = writeln!(file, "[{}.{}]", name, date.format("%Y-%m-%d"));
+        let _ = writeln!(file, "dependents = {}", dependents);
+        let _ = writeln!(file, "created = {}", created.format("%Y-%m-%d"));
+        let _ = writeln!(file, "updated = {}", updated.format("%Y-%m-%d"));
     }
+
+    //println!("|crate|dependents|created_at|updated_at|");
+    //println!("|-----|----------|----------|----------|");
+    //for (dependents, name, created, updated) in result {
+    //    println!(
+    //        "|{}|{}|{}|{}|",
+    //        name,
+    //        dependents,
+    //        created.format("%Y-%m-%d"),
+    //        updated.format("%Y-%m-%d")
+    //    );
+    //}
 }
